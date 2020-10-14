ae8ec544b27fe2c2e04ba54c65f554ff49ff0fdb
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 01:00:09 +0800

    [practice08] [Hannah] feature: added Teacher.java

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,33 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+
+        return klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public String introduce() {
+        if (klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }else
+            return super.introduce() + String.format(" I am a Teacher. I teach " + klass.getDisplayName() + ".");
+    }
+    public String introduceWith(Student student) {
+        if (this.getKlass().getNumber() == student.getKlass().getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName()+ ".";
+        }else
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
 }
+
