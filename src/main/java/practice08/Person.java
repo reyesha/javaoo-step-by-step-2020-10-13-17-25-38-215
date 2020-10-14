c133f99116416a9922e85278ea1f0d6811b308ca
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 00:59:25 +0800

    [practice08] [Hannah] feature: added Person.java

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,39 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.",
+                name, age);
+    }
+
+    @Override
+    public boolean equals(Object myObj){
+        if (this == myObj) return true;
+        if (myObj == null || getClass() != myObj.getClass()) return false;
+        Person person = (Person) myObj;
+        return age == person.age && id == person.id && Objects.equals(name, person.name);
+    }
 }
