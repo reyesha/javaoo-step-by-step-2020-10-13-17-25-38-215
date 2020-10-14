7fafe432d2f05ce7fb3724ba8e51fc8482aafdd6
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 19:31:25 +0800

    [practice04] [Hannah] add: Worker.java

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,18 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+    private int klass;
+    public Worker(String name, int age) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String basicIntroduce = super.basicIntroduce();
+        return basicIntroduce + " I am a Worker. I have a job.";
+    }
 }
+
