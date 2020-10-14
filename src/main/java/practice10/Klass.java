e56ae5ddbc94cc1883b340aef73712ddfeca3480
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 01:03:01 +0800

    [practice10] [Hannah] feature: added Klass.java

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,41 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    int number;
+    Student leader;
+    private List<Student> members = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",number);
+    }
+
+    public void assignLeader(Student leader) {
+        if(members.contains(leader)){
+            this.setLeader(leader);
+        }else
+            System.out.print("It is not one of us.\n");
+    }
+
+    public void appendMember(Student member) {
+        members.add(member);
+    }
 }
