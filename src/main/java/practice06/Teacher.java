f1cee14060217dac5061bcac2795166b490e577d
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 20:23:17 +0800

    [practice06] [Hannah] add: Teacher.java

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public int getKlass() {
+
+        return klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce() {
+        if (klass == 0) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }else
+            return super.introduce() + String.format(" I am a Teacher. I teach Class %d.", klass);
+    }
 }
+
