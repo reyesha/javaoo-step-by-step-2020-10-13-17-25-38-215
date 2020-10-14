c80a3685a8a84c3ea08389e6595c95859385e5f7
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-14 23:15:36 +0800

    [practice07] [Hannah] add: add Klass.java

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,17 @@
 package practice07;
 
 public class Klass {
+    int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",number);
+    }
 }
