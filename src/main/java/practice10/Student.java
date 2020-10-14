ff9f6f861b701f7b710fc1be18c72cedf031aefb
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 01:03:43 +0800

    [practice10] [Hannah] feature: added Student.java

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,27 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private final Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+
+        return klass;
+    }
+
+    public String introduce() {
+        if(klass.getLeader() != null){
+            if (klass.getLeader().getName().equals(this.getName())){
+                return super.introduce() + String.format(" I am a Student. I am Leader of " + klass.getDisplayName() + ".");
+            }
+        }
+
+        return super.introduce() + String.format(" I am a Student. I am at " + klass.getDisplayName() + ".");
+
+    }
 }
+
