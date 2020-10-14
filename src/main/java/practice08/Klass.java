950837a5f880e8d991f91bad8565efe9afb15a1d
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 00:58:36 +0800

    [practice08] [Hannah] feature: added Klass.java

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,30 @@
 package practice08;
 
 public class Klass {
+    int number;
+    Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s",number);
+    }
+
+    public void assignLeader(Student student) {
+        this.setLeader(student);
+    }
 }
