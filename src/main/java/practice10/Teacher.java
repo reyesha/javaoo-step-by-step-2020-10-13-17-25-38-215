fc90a1c2a59c5e028206146095382b4cd7d64e3a
reyesha <71996658+reyesha@users.noreply.github.com>
2020-10-15 08:46:32 +0800

    practice10] [Hannah] feature: updated Teacher.java

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,11 +1,19 @@
 package practice10;
 
+import java.util.LinkedList;
+
 public class Teacher extends Person {
     private Klass klass;
+    private LinkedList<Klass> linkedList = new LinkedList<>();
 
     public Teacher(int id, String name, int age, Klass klass) {
         super(id, name, age);
         this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.linkedList = linkedList;
     }
 
     public Klass getKlass() {
@@ -17,17 +25,30 @@ public class Teacher extends Person {
         super(id, name, age);
     }
 
-    public String introduce() {
-        if (klass == null) {
-            return super.introduce() + " I am a Teacher. I teach No Class.";
-        }else
-            return super.introduce() + String.format(" I am a Teacher. I teach " + klass.getDisplayName() + ".");
+    public LinkedList<Klass> getClasses() {
+        return linkedList;
     }
+
+    public String introduce() {
+        String classes = "";
+        for (int index = 0; index < linkedList.size();index++){
+            classes += linkedList.get(index).getNumber();
+            if(!((linkedList.size() - index) == 1)){
+                classes += ", ";
+            }
+        }
+        return linkedList.size() != 0 ? super.introduce() + " I am a Teacher. I teach Class " + classes + "." : super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+
     public String introduceWith(Student student) {
-        if (this.getKlass().getNumber() == student.getKlass().getNumber()) {
+        if (linkedList.contains( student.getKlass())) {
             return super.introduce() + " I am a Teacher. I teach " + student.getName()+ ".";
         }else
             return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+
+    public boolean isTeaching(Student student) {
+        return linkedList.contains(student.getKlass());
     }
 }
 
